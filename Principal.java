fb34c7918c6dd1413df46342eb0f2fbe29599637
biaalbano <biaahalbano78@gmail.com>
2023-05-02 14:44:06 +0000

    salvar

---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String args[]){
+        Dog dog =new Dog("neguinha",13);
+         
+        System.out.println(dog.getNome());
+        System.out.println(dog.getIdade());
+
+        Cat cat =new Cat("Coxinha",3);
+        
+        System.out.println(cat.getNome());
+        System.out.println(cat.getIdade());
+    }
+}
